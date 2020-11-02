b63e915e912a73081607c3813db29e026e745d54
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-02 22:55:59 +0000

    update repo

--- Mvc4MusicStore/Controllers/CheckoutController.cs
+++ Mvc4MusicStore/Controllers/CheckoutController.cs
@@ -17,4 +17,20 @@ namespace Mvc4MusicStore.Controllers
             return View();
         }
         //
-        // POS
+        // POST: /Checkout/AddressAndPayment
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode,
+                    StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.Username =
