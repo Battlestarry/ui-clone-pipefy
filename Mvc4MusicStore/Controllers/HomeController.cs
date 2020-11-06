338190aa6614b6ddf5813621aaeafc92cfba304b
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-06 06:26:31 +0000

    Cleanup config

--- Mvc4MusicStore/Controllers/HomeController.cs
+++ Mvc4MusicStore/Controllers/HomeController.cs
@@ -9,4 +9,18 @@ namespace Mvc4MusicStore.Controllers
 {
     public class HomeController : Controller
     {
-    
+        //
+        // GET: /Home/
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        public ActionResult Index()
+        {
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            retu
