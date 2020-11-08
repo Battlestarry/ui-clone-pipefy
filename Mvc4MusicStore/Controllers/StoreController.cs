4c63c1a7ced5e7323a3ea8bb8017a3d73e1e65b1
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-08 21:33:26 +0000

    Trying to fix travis

--- Mvc4MusicStore/Controllers/StoreController.cs
+++ Mvc4MusicStore/Controllers/StoreController.cs
@@ -24,4 +24,26 @@ namespace Mvc4MusicStore.Controllers
         {
             // Retrieve Genre and its Associated Albums from database
             var genreModel = storeDB.Genres.Include("Albums")
-               
+                .Single(g => g.Name == genre);
+
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = storeDB.Albums.Find(id);
+
+            return View(album);
+        }
+
+        //
+        // GET: /Store/GenreMenu
+        [ChildActionOnly]
+        public ActionResult GenreMenu()
+        {
+            var genres = storeDB.Genres.ToList();
+            return PartialView(genres);
+        }
+    }
+}
