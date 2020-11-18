a1e037dd0790d2767664bdc483514524933645b1
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-18 10:27:20 +0000

    ANDROID: exec_domains: Disable request_module() call for personalities

---
+++ Mvc4MusicStore/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Mvc4MusicStore.Models
+{
+    [Bind(Exclude = "AlbumId")]
+    p
