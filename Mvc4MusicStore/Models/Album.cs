138220c69342308c7936272baf0ef4396c070715
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-20 14:17:59 +0000

    removed space

--- Mvc4MusicStore/Models/Album.cs
+++ Mvc4MusicStore/Models/Album.cs
@@ -9,4 +9,17 @@ using System.Web.Mvc;
 namespace Mvc4MusicStore.Models
 {
     [Bind(Exclude = "AlbumId")]
-    p
+    public class Album
+    {
+        [ScaffoldColumn(false)]
+        public int AlbumId { get; set; }
+        [DisplayName("Genre")]
+        public int GenreId { get; set; }
+        [DisplayName("Artist")]
+        public int ArtistId { get; set; }
+        [Required(ErrorMessage = "An Album Title is required")]
+        [StringLength(160)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 100.00,
+            ErrorMessage = "Price must be b
