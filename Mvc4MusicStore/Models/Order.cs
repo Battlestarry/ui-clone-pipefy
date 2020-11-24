b7f0108862c9d0af9d6c2ff38455237008e1c8fc
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-24 17:31:28 +0000

    cleanup code.

--- Mvc4MusicStore/Models/Order.cs
+++ Mvc4MusicStore/Models/Order.cs
@@ -10,4 +10,15 @@ namespace Mvc4MusicStore.Models
     {
         [ScaffoldColumn(false)]
         public int OrderId { get; set; }
-       
+        [ScaffoldColumn(false)]
+        public System.DateTime OrderDate { get; set; }
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(160)]
+        public string LastName { 
