e5083cddf744b38332cdff1ac708114a475d6c4d
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-28 19:19:55 +0000

    add generator

--- Mvc4MusicStore/Models/OrderDetail.cs
+++ Mvc4MusicStore/Models/OrderDetail.cs
@@ -7,4 +7,12 @@ namespace Mvc4MusicStore.Models
 {
     public class OrderDetail
     {
-        public int OrderD
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int AlbumId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public virtual Album Album { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
---
+++ Mvc4MusicStore/Models/SampleData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Mvc4MusicStore.Models
+{
+    public class SampleData : DropCreateDatabaseIfModelChanges<MusicStoreEn
