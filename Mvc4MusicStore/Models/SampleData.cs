beee17cf6096cab5ebd1e51c9ed4c0dd83fcb954
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2020-11-29 09:18:44 +0000

    Remove RVM from Travis since we use Ruby in ChefDK

--- Mvc4MusicStore/Models/SampleData.cs
+++ Mvc4MusicStore/Models/SampleData.cs
@@ -6,4 +6,16 @@ using System.Data.Entity;
 
 namespace Mvc4MusicStore.Models
 {
-    public class SampleData : DropCreateDatabaseIfModelChanges<MusicStoreEn
+    public class SampleData : DropCreateDatabaseIfModelChanges<MusicStoreEntities>
+    {
+        protected override void Seed(MusicStoreEntities context)
+        {
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Rock" },
+                new Genre { Name = "Jazz" },
+                new Genre { Name = "Metal" },
+                new Genre { Name = "Alternative" },
+                new Genre { Name = "Disco" },
+                new Genre { Name = "Blues" },
+                new Genre { Name = "La
