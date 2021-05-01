5fbb2eeb0f98ce35ad25cedaa52713a1109d45a0
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-05-01 21:32:03 +0000

    net: usb: cdc_ether: Use wwan interface for Telit modules

--- Mvc4MusicStore/Models/UserGroup.cs
+++ Mvc4MusicStore/Models/UserGroup.cs
@@ -4,3 +4,11 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+namespace Mvc4MusicStore.Models
+{
+    public class UserGroup
+    {
+        [StringLength(16)]
+        public string Name { get; set; }
+    }
+}
