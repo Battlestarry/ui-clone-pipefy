9a8cfb6d02e05d3e3a250d078c7b0c49b09979b1
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-07-16 12:59:07 +0000

    fix rendering

--- Mvc4MusicStore/Properties/AssemblyInfo.cs
+++ Mvc4MusicStore/Properties/AssemblyInfo.cs
@@ -3,4 +3,15 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
-// s
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mvc4MusicStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mvc4MusicStore")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false
