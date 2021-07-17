5ce8bda8d186e39c50a7f1eaf1038079a462e7f1
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-07-17 19:18:06 +0000

    push message

--- Mvc4MusicStore/Properties/AssemblyInfo.cs
+++ Mvc4MusicStore/Properties/AssemblyInfo.cs
@@ -14,4 +14,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64adf8a9-30ab-409b-97cb-b36ca4ed26c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+// 
