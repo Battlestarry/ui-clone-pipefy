1323e4d33e9693d625ebb69cae8c91e805c8cee7
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-07-20 06:45:22 +0000

    Add citation.

---
+++ Mvc4MusicStore/Scripts/jquery-1.8.2-vsdoc.js
@@ -0,0 +1,206 @@
+
+﻿/* NUGET: BEGIN LICENSE TEXT
+ *
+ * Microsoft grants you the right to use these script files for the sole
+ * purpose of either: (i) interacting through your browser with the Microsoft
+ * website or online service, subject to the applicable licensing or use
+ * terms; or (ii) using the files as included with a Microsoft product subject
+ * to that product's license terms. Microsoft reserves all other rights to the
+ * files not expressly granted by Microsoft, whether by implication, estoppel
+ * or otherwise. Insofar as a script file is dual licensed under GPL,
+ * Microsoft neither took the code under GPL nor distributes it thereunder but
+ * under the terms set out in this paragraph. All notices and licenses
+ * below are for informational purposes only.
+ *
+ * NUGET: END LICENSE TEXT */
+/*
+* This file has been generated to support Visual Studio IntelliSense.
+* You should not use this file at runtime inside the browser--it is only
+* intended to be used only for design-time IntelliSense.  Please use the
+* standard jQuery library for all production use.
+*
+* Comment version: 1.8.2
+*/
+
+/*!
+* jQuery JavaScript Library v1.8.2
+* http://jquery.com/
+*
+* Distributed in whole under the terms of the MIT
+*
+* Copyright 2010, John Resig
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* Includes Sizzle.js
+* http://sizzlejs.com/
+* Copyright 2010, The Dojo Foundation
+* Released under the MIT and BSD Licenses.
+*/
+
+(function (window, undefined) {
+    var jQuery = function (selector, context) {
+        /// <summary>
+        ///     1: Accepts a string containing a CSS selector which is then used to match a set of elements.
+        ///     &#10;    1.1 - $(selector, context) 
+        ///     &#10;    1.2 - $(element) 
+        ///     &#10;    1.3 - $(object) 
+        ///     &#10;    1.4 - $(elementArray) 
+        ///     &#10;    1.5 - $(jQuery object) 
+        ///     &#10;    1.6 - $()
+        ///     &#10;2: Creates DOM elements on the fly from the provided string of raw HTML.
+        ///     &#10;    2.1 - $(html, ownerDocument) 
+        ///     &#10;    2.2 - $(html, props)
+        ///     &#10;3: Binds a function to be executed when the DOM has finished loading.
+        ///     &#10;    3.1 - $(callback)
+        /// </summary>
+        /// <param name="selector" type="String">
+        ///     A string containing a selector expression
+        /// </param>
+        /// <param name="context" type="jQuery">
+        ///     A DOM Element, Document, or jQuery to use as context
+        /// </param>
+        /// <returns type="jQuery" />
+
+        // The jQuery object is actually just the init constructor 'enhanced'
+        return new jQuery.fn.init(selector, context, rootjQuery);
+    };
+    jQuery.Animation = function Animation(elem, properties, options) {
+
+        var result,
+            index = 0,
+            tweenerIndex = 0,
+            length = animationPrefilters.length,
+            deferred = jQuery.Deferred().always(function () {
+                // don't match elem in the :animated selector
+                delete tick.elem;
+            }),
+            tick = function () {
+                var currentTime = fxNow || createFxNow(),
+                    remaining = Math.max(0, animation.startTime + animation.duration - currentTime),
+                    percent = 1 - (remaining / animation.duration || 0),
+                    index = 0,
+                    length = animation.tweens.length;
+
+                for (; index < length ; index++) {
+                    animation.tweens[index].run(percent);
+                }
+
+                deferred.notifyWith(elem, [animation, percent, remaining]);
+
+                if (percent < 1 && length) {
+                    return remaining;
+                } else {
+                    deferred.resolveWith(elem, [animation]);
+                    return false;
+                }
+            },
+            animation = deferred.promise({
+                elem: elem,
+                props: jQuery.extend({}, properties),
+                opts: jQuery.extend(true, { specialEasing: {} }, options),
+                originalProperties: properties,
+                originalOptions: options,
+                startTime: fxNow || createFxNow(),
+                duration: options.duration,
+                tweens: [],
+                createTween: function (prop, end, easing) {
+                    var tween = jQuery.Tween(elem, animation.opts, prop, end,
+                            animation.opts.specialEasing[prop] || animation.opts.easing);
+                    animation.tweens.push(tween);
+                    return tween;
+                },
+                stop: function (gotoEnd) {
+                    var index = 0,
+                        // if we are going to the end, we want to run all the tweens
+                        // otherwise we skip this part
+                        length = gotoEnd ? animation.tweens.length : 0;
+
+                    for (; index < length ; index++) {
+                        animation.tweens[index].run(1);
+                    }
+
+                    // resolve when we played the last frame
+                    // otherwise, reject
+                    if (gotoEnd) {
+                        deferred.resolveWith(elem, [animation, gotoEnd]);
+                    } else {
+                        deferred.rejectWith(elem, [animation, gotoEnd]);
+                    }
+                    return this;
+                }
+            }),
+            props = animation.props;
+
+        propFilter(props, animation.opts.specialEasing);
+
+        for (; index < length ; index++) {
+            result = animationPrefilters[index].call(animation, elem, props, animation.opts);
+            if (result) {
+                return result;
+            }
+        }
+
+        createTweens(animation, props);
+
+        if (jQuery.isFunction(animation.opts.start)) {
+            animation.opts.start.call(elem, animation);
+        }
+
+        jQuery.fx.timer(
+            jQuery.extend(tick, {
+                anim: animation,
+                queue: animation.opts.queue,
+                elem: elem
+            })
+        );
+
+        // attach callbacks from options
+        return animation.progress(animation.opts.progress)
+            .done(animation.opts.done, animation.opts.complete)
+            .fail(animation.opts.fail)
+            .always(animation.opts.always);
+    };
+    jQuery.Callbacks = function (options) {
+        /// <summary>
+        ///     A multi-purpose callbacks list object that provides a powerful way to manage callback lists.
+        /// </summary>
+        /// <param name="options" type="String">
+        ///     An optional list of space-separated flags that change how the callback list behaves.
+        /// </param>
+
+
+        // Convert options from String-formatted to Object-formatted if needed
+        // (we check in cache first)
+        options = typeof options === "string" ?
+            (optionsCache[options] || createOptions(options)) :
+            jQuery.extend({}, options);
+
+        var // Last fire value (for non-forgettable lists)
+            memory,
+            // Flag to know if list was already fired
+            fired,
+            // Flag to know if list is currently firing
+            firing,
+            // First callback to fire (used internally by add and fireWith)
+            firingStart,
+            // End of the loop when firing
+            firingLength,
+            // Index of currently firing callback (modified by remove if needed)
