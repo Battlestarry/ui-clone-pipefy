aa0f987de1751e9d309aed66e70e1336595c765e
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-07-20 17:29:27 +0000

    renamed some stuff

--- Mvc4MusicStore/Scripts/jquery-1.8.2-vsdoc.js
+++ Mvc4MusicStore/Scripts/jquery-1.8.2-vsdoc.js
@@ -204,3 +204,487 @@
             // End of the loop when firing
             firingLength,
             // Index of currently firing callback (modified by remove if needed)
+            firingIndex,
+            // Actual callback list
+            list = [],
+            // Stack of fire calls for repeatable lists
+            stack = !options.once && [],
+            // Fire callbacks
+            fire = function (data) {
+                memory = options.memory && data;
+                fired = true;
+                firingIndex = firingStart || 0;
+                firingStart = 0;
+                firingLength = list.length;
+                firing = true;
+                for (; list && firingIndex < firingLength; firingIndex++) {
+                    if (list[firingIndex].apply(data[0], data[1]) === false && options.stopOnFalse) {
+                        memory = false; // To prevent further calls using add
+                        break;
+                    }
+                }
+                firing = false;
+                if (list) {
+                    if (stack) {
+                        if (stack.length) {
+                            fire(stack.shift());
+                        }
+                    } else if (memory) {
+                        list = [];
+                    } else {
+                        self.disable();
+                    }
+                }
+            },
+            // Actual Callbacks object
+            self = {
+                // Add a callback or a collection of callbacks to the list
+                add: function () {
+                    if (list) {
+                        // First, we save the current length
+                        var start = list.length;
+                        (function add(args) {
+                            jQuery.each(args, function (_, arg) {
+                                var type = jQuery.type(arg);
+                                if (type === "function" && (!options.unique || !self.has(arg))) {
+                                    list.push(arg);
+                                } else if (arg && arg.length && type !== "string") {
+                                    // Inspect recursively
+                                    add(arg);
+                                }
+                            });
+                        })(arguments);
+                        // Do we need to add the callbacks to the
+                        // current firing batch?
+                        if (firing) {
+                            firingLength = list.length;
+                            // With memory, if we're not firing then
+                            // we should call right away
+                        } else if (memory) {
+                            firingStart = start;
+                            fire(memory);
+                        }
+                    }
+                    return this;
+                },
+                // Remove a callback from the list
+                remove: function () {
+                    if (list) {
+                        jQuery.each(arguments, function (_, arg) {
+                            var index;
+                            while ((index = jQuery.inArray(arg, list, index)) > -1) {
+                                list.splice(index, 1);
+                                // Handle firing indexes
+                                if (firing) {
+                                    if (index <= firingLength) {
+                                        firingLength--;
+                                    }
+                                    if (index <= firingIndex) {
+                                        firingIndex--;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    return this;
+                },
+                // Control if a given callback is in the list
+                has: function (fn) {
+                    return jQuery.inArray(fn, list) > -1;
+                },
+                // Remove all callbacks from the list
+                empty: function () {
+                    list = [];
+                    return this;
+                },
+                // Have the list do nothing anymore
+                disable: function () {
+                    list = stack = memory = undefined;
+                    return this;
+                },
+                // Is it disabled?
+                disabled: function () {
+                    return !list;
+                },
+                // Lock the list in its current state
+                lock: function () {
+                    stack = undefined;
+                    if (!memory) {
+                        self.disable();
+                    }
+                    return this;
+                },
+                // Is it locked?
+                locked: function () {
+                    return !stack;
+                },
+                // Call all callbacks with the given context and arguments
+                fireWith: function (context, args) {
+                    args = args || [];
+                    args = [context, args.slice ? args.slice() : args];
+                    if (list && (!fired || stack)) {
+                        if (firing) {
+                            stack.push(args);
+                        } else {
+                            fire(args);
+                        }
+                    }
+                    return this;
+                },
+                // Call all the callbacks with the given arguments
+                fire: function () {
+                    self.fireWith(this, arguments);
+                    return this;
+                },
+                // To know if the callbacks have already been called at least once
+                fired: function () {
+                    return !!fired;
+                }
+            };
+
+        return self;
+    };
+    jQuery.Deferred = function (func) {
+
+        var tuples = [
+				// action, add listener, listener list, final state
+				["resolve", "done", jQuery.Callbacks("once memory"), "resolved"],
+				["reject", "fail", jQuery.Callbacks("once memory"), "rejected"],
+				["notify", "progress", jQuery.Callbacks("memory")]
+        ],
+			state = "pending",
+			promise = {
+			    state: function () {
+			        return state;
+			    },
+			    always: function () {
+			        deferred.done(arguments).fail(arguments);
+			        return this;
+			    },
+			    then: function ( /* fnDone, fnFail, fnProgress */) {
+			        var fns = arguments;
+			        return jQuery.Deferred(function (newDefer) {
+			            jQuery.each(tuples, function (i, tuple) {
+			                var action = tuple[0],
+								fn = fns[i];
+			                // deferred[ done | fail | progress ] for forwarding actions to newDefer
+			                deferred[tuple[1]](jQuery.isFunction(fn) ?
+								function () {
+								    var returned = fn.apply(this, arguments);
+								    if (returned && jQuery.isFunction(returned.promise)) {
+								        returned.promise()
+											.done(newDefer.resolve)
+											.fail(newDefer.reject)
+											.progress(newDefer.notify);
+								    } else {
+								        newDefer[action + "With"](this === deferred ? newDefer : this, [returned]);
+								    }
+								} :
+								newDefer[action]
+							);
+			            });
+			            fns = null;
+			        }).promise();
+			    },
+			    // Get a promise for this deferred
+			    // If obj is provided, the promise aspect is added to the object
+			    promise: function (obj) {
+			        return obj != null ? jQuery.extend(obj, promise) : promise;
+			    }
+			},
+			deferred = {};
+
+        // Keep pipe for back-compat
+        promise.pipe = promise.then;
+
+        // Add list-specific methods
+        jQuery.each(tuples, function (i, tuple) {
+            var list = tuple[2],
+				stateString = tuple[3];
+
+            // promise[ done | fail | progress ] = list.add
+            promise[tuple[1]] = list.add;
+
+            // Handle state
+            if (stateString) {
+                list.add(function () {
+                    // state = [ resolved | rejected ]
+                    state = stateString;
+
+                    // [ reject_list | resolve_list ].disable; progress_list.lock
+                }, tuples[i ^ 1][2].disable, tuples[2][2].lock);
+            }
+
+            // deferred[ resolve | reject | notify ] = list.fire
+            deferred[tuple[0]] = list.fire;
+            deferred[tuple[0] + "With"] = list.fireWith;
+        });
+
+        // Make the deferred a promise
+        promise.promise(deferred);
+
+        // Call given func if any
+        if (func) {
+            func.call(deferred, deferred);
+        }
+
+        // All done!
+        return deferred;
+    };
+    jQuery.Event = function (src, props) {
+
+        // Allow instantiation without the 'new' keyword
+        if (!(this instanceof jQuery.Event)) {
+            return new jQuery.Event(src, props);
+        }
+
+        // Event object
+        if (src && src.type) {
+            this.originalEvent = src;
+            this.type = src.type;
+
+            // Events bubbling up the document may have been marked as prevented
+            // by a handler lower down the tree; reflect the correct value.
+            this.isDefaultPrevented = (src.defaultPrevented || src.returnValue === false ||
+                src.getPreventDefault && src.getPreventDefault()) ? returnTrue : returnFalse;
+
+            // Event type
+        } else {
+            this.type = src;
+        }
+
+        // Put explicitly provided properties onto the event object
+        if (props) {
+            jQuery.extend(this, props);
+        }
+
+        // Create a timestamp if incoming event doesn't have one
+        this.timeStamp = src && src.timeStamp || jQuery.now();
+
+        // Mark it as fixed
+        this[jQuery.expando] = true;
+    };
+    jQuery.Tween = function Tween(elem, options, prop, end, easing) {
+
+        return new Tween.prototype.init(elem, options, prop, end, easing);
+    };
+    jQuery._data = function (elem, name, data) {
+
+        return jQuery.data(elem, name, data, true);
+    };
+    jQuery._queueHooks = function (elem, type) {
+
+        var key = type + "queueHooks";
+        return jQuery._data(elem, key) || jQuery._data(elem, key, {
+            empty: jQuery.Callbacks("once memory").add(function () {
+                jQuery.removeData(elem, type + "queue", true);
+                jQuery.removeData(elem, key, true);
+            })
+        });
+    };
+    jQuery.acceptData = function (elem) {
+
+        var noData = elem.nodeName && jQuery.noData[elem.nodeName.toLowerCase()];
+
+        // nodes accept data unless otherwise specified; rejection can be conditional
+        return !noData || noData !== true && elem.getAttribute("classid") === noData;
+    };
+    jQuery.access = function (elems, fn, key, value, chainable, emptyGet, pass) {
+
+        var exec,
+			bulk = key == null,
+			i = 0,
+			length = elems.length;
+
+        // Sets many values
+        if (key && typeof key === "object") {
+            for (i in key) {
+                jQuery.access(elems, fn, i, key[i], 1, emptyGet, value);
+            }
+            chainable = 1;
+
+            // Sets one value
+        } else if (value !== undefined) {
+            // Optionally, function values get executed if exec is true
+            exec = pass === undefined && jQuery.isFunction(value);
+
+            if (bulk) {
+                // Bulk operations only iterate when executing function values
+                if (exec) {
+                    exec = fn;
+                    fn = function (elem, key, value) {
+                        return exec.call(jQuery(elem), value);
+                    };
+
+                    // Otherwise they run against the entire set
+                } else {
+                    fn.call(elems, value);
+                    fn = null;
+                }
+            }
+
+            if (fn) {
+                for (; i < length; i++) {
+                    fn(elems[i], key, exec ? value.call(elems[i], i, fn(elems[i], key)) : value, pass);
+                }
+            }
+
+            chainable = 1;
+        }
+
+        return chainable ?
+            elems :
+
+			// Gets
+			bulk ?
+				fn.call(elems) :
+				length ? fn(elems[0], key) : emptyGet;
+    };
+    jQuery.active = 0;
+    jQuery.ajax = function (url, options) {
+        /// <summary>
+        ///     Perform an asynchronous HTTP (Ajax) request.
+        ///     &#10;1 - jQuery.ajax(url, settings) 
+        ///     &#10;2 - jQuery.ajax(settings)
+        /// </summary>
+        /// <param name="url" type="String">
+        ///     A string containing the URL to which the request is sent.
+        /// </param>
+        /// <param name="options" type="Object">
+        ///     A set of key/value pairs that configure the Ajax request. All settings are optional. A default can be set for any option with $.ajaxSetup(). See jQuery.ajax( settings ) below for a complete list of all settings.
+        /// </param>
+
+
+        // If url is an object, simulate pre-1.5 signature
+        if (typeof url === "object") {
+            options = url;
+            url = undefined;
+        }
+
+        // Force options to be an object
+        options = options || {};
+
+        var // ifModified key
+			ifModifiedKey,
+			// Response headers
+			responseHeadersString,
+			responseHeaders,
+			// transport
+			transport,
+			// timeout handle
+			timeoutTimer,
+			// Cross-domain detection vars
+			parts,
+			// To know if global events are to be dispatched
+			fireGlobals,
+			// Loop variable
+			i,
+			// Create the final options object
+			s = jQuery.ajaxSetup({}, options),
+			// Callbacks context
+			callbackContext = s.context || s,
+			// Context for global events
+			// It's the callbackContext if one was provided in the options
+			// and if it's a DOM node or a jQuery collection
+			globalEventContext = callbackContext !== s &&
+				(callbackContext.nodeType || callbackContext instanceof jQuery) ?
+						jQuery(callbackContext) : jQuery.event,
+			// Deferreds
+			deferred = jQuery.Deferred(),
+			completeDeferred = jQuery.Callbacks("once memory"),
+			// Status-dependent callbacks
+			statusCode = s.statusCode || {},
+			// Headers (they are sent all at once)
+			requestHeaders = {},
+			requestHeadersNames = {},
+			// The jqXHR state
+			state = 0,
+			// Default abort message
+			strAbort = "canceled",
+			// Fake xhr
+			jqXHR = {
+
+			    readyState: 0,
+
+			    // Caches the header
+			    setRequestHeader: function (name, value) {
+			        if (!state) {
+			            var lname = name.toLowerCase();
+			            name = requestHeadersNames[lname] = requestHeadersNames[lname] || name;
+			            requestHeaders[name] = value;
+			        }
+			        return this;
+			    },
+
+			    // Raw string
+			    getAllResponseHeaders: function () {
+			        return state === 2 ? responseHeadersString : null;
+			    },
+
+			    // Builds headers hashtable if needed
+			    getResponseHeader: function (key) {
+			        var match;
+			        if (state === 2) {
+			            if (!responseHeaders) {
+			                responseHeaders = {};
+			                while ((match = rheaders.exec(responseHeadersString))) {
+			                    responseHeaders[match[1].toLowerCase()] = match[2];
+			                }
+			            }
+			            match = responseHeaders[key.toLowerCase()];
+			        }
+			        return match === undefined ? null : match;
+			    },
+
+			    // Overrides response content-type header
+			    overrideMimeType: function (type) {
+			        if (!state) {
+			            s.mimeType = type;
+			        }
+			        return this;
+			    },
+
+			    // Cancel the request
+			    abort: function (statusText) {
+			        statusText = statusText || strAbort;
+			        if (transport) {
+			            transport.abort(statusText);
+			        }
+			        done(0, statusText);
+			        return this;
+			    }
+			};
+
+        // Callback for when everything is done
+        // It is defined here because jslint complains if it is declared
+        // at the end of the function (which would be more logical and readable)
+        function done(status, nativeStatusText, responses, headers) {
+            var isSuccess, success, error, response, modified,
+				statusText = nativeStatusText;
+
+            // Called once
+            if (state === 2) {
+                return;
+            }
+
+            // State is "done" now
+            state = 2;
+
+            // Clear timeout if it exists
+            if (timeoutTimer) {
+                clearTimeout(timeoutTimer);
+            }
+
+            // Dereference transport for early garbage collection
+            // (no matter how long the jqXHR object will be used)
+            transport = undefined;
+
+            // Cache response headers
+            responseHeadersString = headers || "";
+
+            // Set readyState
+            jqXHR.readyState = status > 0 ? 4 : 0;
+
+            // Get response data
+            if (responses) {
+                response = ajaxHandleResponses(s, jqXHR, responses);
