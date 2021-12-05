8a3646bc4fcce526b5e39d0e221f5c0c8998fd50
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2021-12-05 18:42:45 +0000

    fixed readme file

--- Mvc4MusicStore/Scripts/knockout-2.2.0.js
+++ Mvc4MusicStore/Scripts/knockout-2.2.0.js
@@ -9,4 +9,12 @@
  * or otherwise. Insofar as a script file is dual licensed under GPL,
  * Microsoft neither took the code under GPL nor distributes it thereunder but
  * under the terms set out in this paragraph. All notices and licenses
- * below are 
+ * below are for informational purposes only.
+ *
+ * NUGET: END LICENSE TEXT */
+// Knockout JavaScript library v2.2.0
+// (c) Steven Sanderson - http://knockoutjs.com/
+// License: MIT (http://www.opensource.org/licenses/mit-license.php)
+
+(function() {function i(v){throw v;}var l=!0,n=null,q=!1;function t(v){return function(){return v}};var w=window,x=document,fa=navigator,E=window.jQuery,H=void 0;
+function K(v){function ga(a,d,c,e,f){var g=[],a=b.j(function(){var a=d(c,f)||[];0<g.length&&(b.a.Xa(L(g),a),e&&b.r
