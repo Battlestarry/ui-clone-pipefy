b0b0f2ae0860bc46051ff9e23774164a9c3db834
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-04-30 16:54:54 +0000

    Maven artifacts for GOAL Runtime version \t\t\t\t\t\t1.1.5-SNAPSHOT

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -34,4 +34,15 @@
  * the current UA and makes the results available to you in two ways:
  * as properties on a global Modernizr object, and as classes on the
  * <html> element. This information allows you to progressively enhance
- * your pages with a granular level of control over 
+ * your pages with a granular level of control over the experience.
+ *
+ * Modernizr has an optional (not included) conditional resource loader
+ * called Modernizr.load(), based on Yepnope.js (yepnopejs.com).
+ * To get a build that includes Modernizr.load(), as well as choosing
+ * which tests to include, go to www.modernizr.com/download/
+ *
+ * Authors        Faruk Ates, Paul Irish, Alex Sexton
+ * Contributors   Ryan Seddon, Ben Alman
+ */
+
+window.Modernizr = (function( window, docu
