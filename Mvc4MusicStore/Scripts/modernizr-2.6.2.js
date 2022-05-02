3b17e0597b50208329a321337183ad183ae9edfb
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-05-02 12:17:23 +0000

    jj

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -84,4 +84,17 @@ window.Modernizr = (function( window, document, undefined ) {
 
     /*>>domprefixes*/
     // Following spec is to expose vendor-specific style properties as:
-    //   elem.style.WebkitBord
+    //   elem.style.WebkitBorderRadius
+    // and the following would be incorrect:
+    //   elem.style.webkitBorderRadius
+
+    // Webkit ghosts their properties in lowercase but Opera & Moz do not.
+    // Microsoft uses a lowercase `ms` instead of the correct `Ms` in IE8+
+    //   erik.eae.net/archives/2008/03/10/21.48.10/
+
+    // More here: github.com/Modernizr/Modernizr/issues/issue/21
+    omPrefixes = 'Webkit Moz O ms',
+
+    cssomPrefixes = omPrefixes.split(' '),
+
+    domPrefixes = omPrefixes.toLowerCase().split(' '
