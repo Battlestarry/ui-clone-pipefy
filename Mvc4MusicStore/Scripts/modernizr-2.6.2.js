ad7ef799a1c2bd8b645113d115a5504ffd6f4278
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-05-15 21:37:44 +0000

    code style fix

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -269,4 +269,22 @@ window.Modernizr = (function( window, document, undefined ) {
 
         var target = this;
 
-        if (typeof t
+        if (typeof target != "function") {
+            throw new TypeError();
+        }
+
+        var args = slice.call(arguments, 1),
+            bound = function () {
+
+            if (this instanceof bound) {
+
+              var F = function(){};
+              F.prototype = target.prototype;
+              var self = new F();
+
+              var result = target.apply(
+                  self,
+                  args.concat(slice.call(arguments))
+              );
+              if (Object(result) === result) {
+             
