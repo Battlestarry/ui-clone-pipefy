83fca0ee3e2c390315fb6dcb17b705c22e02f31c
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-05-21 11:41:51 +0000

    Change-logging

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -401,4 +401,13 @@ window.Modernizr = (function( window, document, undefined ) {
     function testPropsAll( prop, prefixed, elem ) {
 
         var ucProp  = prop.charAt(0).toUpperCase() + prop.slice(1),
-            
+            props   = (prop + ' ' + cssomPrefixes.join(ucProp + ' ') + ucProp).split(' ');
+
+        // did they call .prefixed('boxSizing') or are we just testing a prop?
+        if(is(prefixed, "string") || is(prefixed, "undefined")) {
+          return testProps(props, prefixed);
+
+        // otherwise, they called .prefixed('requestAnimationFrame', window[, elem])
+        } else {
+          props = (prop + ' ' + (domPrefixes).join(ucProp + ' ') + ucProp).split(' ');
+          return 
