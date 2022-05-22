3be2e11a6b66bf7fe92437013615c352e29d1421
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-05-22 11:54:31 +0000

    Update Swedish translation.

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -435,4 +435,17 @@ window.Modernizr = (function( window, document, undefined ) {
         return testPropsAll('boxDirection');
     };
 
-    // On the S60 and BB Storm, getContex
+    // On the S60 and BB Storm, getContext exists, but always returns undefined
+    // so we actually have to call getContext() to verify
+    // github.com/Modernizr/Modernizr/issues/issue/97/
+
+    tests['canvas'] = function() {
+        var elem = document.createElement('canvas');
+        return !!(elem.getContext && elem.getContext('2d'));
+    };
+
+    tests['canvastext'] = function() {
+        return !!(Modernizr['canvas'] && is(document.createElement('canvas').getContext('2d').fillText, 'function'));
+    };
+
+    // webk.it/70117 is tracking a legit We
