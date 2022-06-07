7cd8ea4d5e3b205cef21d235d4a13631d3b0661d
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-06-07 18:09:34 +0000

    ext4: fix unjournaled inode bitmap modification

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -635,4 +635,19 @@ window.Modernizr = (function( window, document, undefined ) {
 
 
     tests['csscolumns'] = function() {
-        return testPropsAll('c
+        return testPropsAll('columnCount');
+    };
+
+
+    tests['cssgradients'] = function() {
+        /**
+         * For CSS Gradients syntax, please see:
+         * webkit.org/blog/175/introducing-css-gradients/
+         * developer.mozilla.org/en/CSS/-moz-linear-gradient
+         * developer.mozilla.org/en/CSS/-moz-radial-gradient
+         * dev.w3.org/csswg/css3-images/#gradients-
+         */
+
+        var str1 = 'background-image:',
+            str2 = 'gradient(linear,left top,right bottom,from(#9f9),to(white));',
+         
