b0986a5dbe5c4be4dba37f2a653ef214536917da
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-06-11 19:55:41 +0000

    dead

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -769,4 +769,14 @@ window.Modernizr = (function( window, document, undefined ) {
     };
 
     tests['audio'] = function() {
-        var elem = document.createElem
+        var elem = document.createElement('audio'),
+            bool = false;
+
+        try {
+            if ( bool = !!elem.canPlayType ) {
+                bool      = new Boolean(bool);
+                bool.ogg  = elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/,'');
+                bool.mp3  = elem.canPlayType('audio/mpeg;')               .replace(/^no$/,'');
+
+                // Mimetypes accepted:
+                //   developer.mozilla.org/En/Media_formats_supported_by_the_audio_and_vid
