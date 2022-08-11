5adf9841ab6be382d0fbf02229200c60c505e32e
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-08-11 13:59:02 +0000

    path correction

--- Mvc4MusicStore/Scripts/modernizr-2.6.2.js
+++ Mvc4MusicStore/Scripts/modernizr-2.6.2.js
@@ -1231,3 +1231,14 @@ window.Modernizr = (function( window, document, undefined ) {
         if (!ownerDocument) {
             ownerDocument = document;
         }
+        var data = getExpandoData(ownerDocument);
+
+        if (html5.shivCSS && !supportsHtml5Styles && !data.hasCSS) {
+          data.hasCSS = !!addStyleSheet(ownerDocument,
+            // corrects block display not defined in IE6/7/8/9
+            'article,aside,figcaption,figure,footer,header,hgroup,nav,section{display:block}' +
+            // adds styling not present in IE6/7/8/9
+            'mark{background:#FF0;color:#000}'
+          );
+        }
+        if (!supports
