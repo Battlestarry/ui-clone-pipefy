6dab202d957c07944c2d7a80aeb9cf05723321e6
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2022-08-31 12:16:07 +0000

    generate pdf

---
+++ Mvc4MusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using Mvc4MusicStore.Models;
+
+namespace Mvc4MusicStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartI
