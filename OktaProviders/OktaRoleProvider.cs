2fdbfe4861ea93e9ad10856a6467052a42bfec70
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2023-02-25 20:43:59 +0000

    Update configuration.php

---
+++ OktaProviders/OktaRoleProvider.cs
@@ -0,0 +1,17 @@
+﻿// Install Okta.Core via the "Okta SDK" which is available on NuGet
+using Okta.Core;
+using Okta.Core.Clients;
+using Okta.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration.Provider; // For the ProviderException thrown by DeleteRole
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace OktaProviders
+{
+    /// <summary>
+    /// Exten
