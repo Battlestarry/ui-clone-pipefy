14544ab4fe1537d8f349f392d017fdd518d6cfd0
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2023-04-19 15:05:19 +0000

    Add latest Ansible versions (2.5.10, 2.5.11, 2.6.7)

--- OktaProviders/Properties/AssemblyInfo.cs
+++ OktaProviders/Properties/AssemblyInfo.cs
@@ -1,3 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtim
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OktaProviders")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OktaProviders")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[ass
