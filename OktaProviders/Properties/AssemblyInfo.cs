48402505ef0dd9dca47406635d0c84c67ebab621
Battlestarry <124610987+Battlestarry@users.noreply.github.com>
2023-04-19 16:07:27 +0000

    rbd: only reset capacity when pointing to head

--- OktaProviders/Properties/AssemblyInfo.cs
+++ OktaProviders/Properties/AssemblyInfo.cs
@@ -12,4 +12,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("OktaProviders")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
-[ass
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8973c2aa-c3f6-4d94-b1f0-867670daaef2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      
